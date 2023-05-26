a9c58798185d21b4a993d7d52922f92c390555f3
Ully Kharisma Putra <ullykharisma@gmail.com>
2023-05-26 19:17:17 +0700

    Feature Dashboard Total Participant

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $total_participant = User::where('role','participant')->count();
+
+        return view('home.index',compact('total_participant'));
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,10 +3,25 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-      <h1>Blank Page</h1>
+      <h1>Dashboard</h1>
     </div>
 
     <div class="section-body">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-secondary">
+                    <i class="far fa-user"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Peserta</h4>
+                    </div>
+                    <div class="card-body">
+                    {{ number_format($total_participant) }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
